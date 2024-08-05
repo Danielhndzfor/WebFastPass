b4c1ecb4883b9d0a3ee895cde888cda53a916ea9
Danielhndzfor <87097302+Danielhndzfor@users.noreply.github.com>
2024-08-05 17:35:17 -0600

    Agregando boton en parallax

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -4,12 +4,18 @@ import '../css/Parallax.css'; // Asegúrate de que la ruta sea correcta
 
 function Parallax() {
     const { t } = useTranslation();
+    const whatsappNumber = '523143526003'; // Reemplaza con el número de WhatsApp de destino
+    const textMessage = `Buenas tardes, me gustaría realizar una cita 📅🕒`;
+    const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(textMessage)}`;
 
     return (
         <div className="parallax-container fade-in">
             <div className="parallax-content">
                 <h1>{t('Uniendo Familias')}</h1>
                 <p>{t('Especialistas en visas y migración')}</p>
+                <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className="contactbtn">
+                    {t('Contáctanos')}
+                </a>
             </div>
         </div>
     );
