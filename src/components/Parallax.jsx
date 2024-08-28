cfb87957ff9686cadd84e02055802719a1dd76ca
Danielhndzfor <87097302+Danielhndzfor@users.noreply.github.com>
2024-08-27 20:47:43 -0600

    Probando boton de parallax

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -4,16 +4,13 @@ import '../css/Parallax.css';
 
 function Parallax({ backgroundImage, title, subtitle, linkText }) {
     const { t } = useTranslation();
-    const whatsappNumber = '523143526003';
-    const textMessage = `Buenas tardes, me gustaría realizar una cita 📅🕒`;
-    const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(textMessage)}`;
 
     return (
         <div className="parallax-container fade-in" style={{ backgroundImage: `url(${backgroundImage})` }}>
             <div className="parallax-content">
                 <h1>{t(title)}</h1>
                 <p>{t(subtitle)}</p>
-                <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className="contactbtn">
+                <a href="/contact" className="contactbtn">
                     {t(linkText)}
                 </a>
             </div>
