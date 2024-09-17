191395ec92a1cce891bd31caaaa9672b676f9f01
Danielhndzfor <87097302+Danielhndzfor@users.noreply.github.com>
2024-09-16 20:29:13 -0600

    moviendo css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next';
 import i18n from './tranduccion/i18n.js';
 import App from './App.jsx'
-import './Index.css'
+import './css/Index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
